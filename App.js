086d5a25cc74e54a32063301f2c38027aa8b49bf
MineLokito <kealvillegases@ittepic.edu.mx>
2020-08-28 11:16:30 -0600

    Se Agrego el Menu de botones abajo

--- App.js
+++ App.js
@@ -1,9 +1,16 @@
+import React from 'react';
 import {createAppContainer, createSwitchNavigator} from 'react-navigation'
-import {createStackNavigator} from 'react-navigation-stack'
+import {createStackNavigator, HeaderBackground} from 'react-navigation-stack'
+import {createBottomTabNavigator} from 'react-navigation-tabs'
+import {Ionicons} from '@expo/vector-icons'
 import LoadingScreen from './Screens/LoadingScreen'
 import HomeScreen from './Screens/HomeScreen'
 import LoginScreen from './Screens/LoginScreen'
 import RegisterScreen from './Screens/RegisterScreen'
+import MessageScreen from "./Screens/MessageScreen"
+import NotificationScreen from "./Screens/NotificationScreen"
+import PostScreen from "./Screens/PostSreen"
+import ProfileScreen from "./Screens/ProfileScreen"
 
 import * as firebase from 'firebase'
 
@@ -16,11 +23,42 @@ var firebaseConfig = {
   messagingSenderId: "364110053039",
   appId: "1:364110053039:web:a0da4653c9b9711f3ade5a"
 };
+
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
- const AppStack = createStackNavigator({
-   home: HomeScreen
- })
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}
+
+ const AppTabNavigator = createBottomTabNavigator(
+   {
+     Inicio: {
+       screen: HomeScreen,
+       navigationOptions: {
+         tabBarIcon:({tintColor}) => <Ionicons name='md-pricetags' size={24} color={tintColor} />
+       }
+     },
+     Anuncios: {
+      screen: PostScreen,
+      navigationOptions: {
+        tabBarIcon:({tintColor}) => <Ionicons name='md-megaphone' size={24} color={tintColor}  />
+      }
+    },
+    Chat: {
+      screen: MessageScreen,
+      navigationOptions: {
+        tabBarIcon:({tintColor}) => <Ionicons name='md-chatbubbles' size={24} color={tintColor}  />
+      }
+    }
+   },
+   {
+     tabBarOptions: {
+      activeBackgroundColor:'#CB2436',
+      inactiveBackgroundColor:'#CB2436',
+      activeTintColor: '#9B9B9B',
+      inactiveTintColor: 'white',
+     }
+   }
+ );
 
  const AuthStack = createStackNavigator({
    Login: LoginScreen,
@@ -28,9 +66,10 @@ firebase.initializeApp(firebaseConfig);
  })
 
  export default createAppContainer(
-   createSwitchNavigator({
+   createSwitchNavigator(
+     {
     Loading: LoadingScreen,
-    App: AppStack,
+    App: AppTabNavigator,
     Auth: AuthStack
       },
       {
--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,6 +1,7 @@
-import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from "react-native"
+import  React from 'react';
+import { View, Text, StyleSheet, TouchableOpacity, LayoutAnimation } from "react-native"
 import * as firebase from 'firebase'
+
 
 export default class HomeScreen extends React.Component {
     state = {
@@ -19,6 +20,7 @@ export default class HomeScreen extends React.Component {
     
 
     render() {
+      LayoutAnimation.easeInEaseOut();
         return (
             <View style={styles.container}>
                 <Text>Hola {this.state.email}!</Text>
@@ -40,5 +42,4 @@ const styles = StyleSheet.create({
 
     }
 });
-
 
